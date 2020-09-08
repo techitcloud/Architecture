c49ae43a0919c077d829e0c43f1fa4afb6582217
rafaelfgx <rafaelfgx@gmail.com>
2020-09-08 20:34:07 -0300

    .

---
+++ source/Model/User/UserModelValidator.cs
@@ -0,0 +1,32 @@
+using FluentValidation;
+
+namespace Architecture.Model
+{
+    public abstract class UserModelValidator : AbstractValidator<UserModel>
+    {
+        public void Auth()
+        {
+            RuleFor(x => x.Auth).SetValidator(new AuthModelValidator());
+        }
+
+        public void Email()
+        {
+            RuleFor(x => x.Email).EmailAddress();
+        }
+
+        public void Forename()
+        {
+            RuleFor(x => x.Forename).NotEmpty();
+        }
+
+        public void Id()
+        {
+            RuleFor(x => x.Id).NotEmpty();
+        }
+
+        public void Surname()
+        {
+            RuleFor(x => x.Surname).NotEmpty();
+        }
+    }
+}
